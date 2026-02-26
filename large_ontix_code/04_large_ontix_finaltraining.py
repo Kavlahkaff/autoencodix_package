8e937cd9982bac7bd6737e05dd46115c294461a0
Jan Ewald <ewald@scadsnb60.informatik.informatik.uni-leipzig.de>
2026-02-26 14:32:00 +0100

    Correct wrong epoch number

--- large_ontix_code/04_large_ontix_finaltraining.py
+++ large_ontix_code/04_large_ontix_finaltraining.py
@@ -59,8 +59,8 @@ acx_container = keep_features_from_acxcontainer(acx_container, ont_lvl2['feature
 
 scconfig = OntixConfig(
 	## Fixed params
-	# epochs=best_hyperparams['config_epochs'],
-	epochs=5, # Reduce for testing
+	epochs=best_hyperparams['config_epochs'],
+	# epochs=5, # Reduce for testing
 	checkpoint_interval= best_hyperparams['config_checkpoint_interval'],
 	loss_reduction= best_hyperparams['config_loss_reduction'],
 	## Tunable params
