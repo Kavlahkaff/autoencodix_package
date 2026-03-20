143f4cabf4db60ad82802cb44a6a10f7f595fe2d
Jan Ewald <ewald@scadsnb60.informatik.informatik.uni-leipzig.de>
2026-03-20 14:26:13 +0100

    Fix wrong epoch number for large varix tuning

--- large_ontix_code/03b_large_varix_tuning.py
+++ large_ontix_code/03b_large_varix_tuning.py
@@ -206,8 +206,8 @@ def syne_trainer(
 
 
 # Hyperparameter configuration 
-# epoch = 250  # For testing, reduce number of epochs
-epoch = 10
+epoch = 250  # For testing, reduce number of epochs
+# epoch = 10
 config_space = {
 	## Fixed params
 	"epochs": epoch,
